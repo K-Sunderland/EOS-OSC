70a34a7da87981edd655172d9dea3ad0c2cd401e
Kyle Sunderland <kyle.sunderland0@gmail.com>
2022-01-10 18:34:08 -0700

    fix osc issues

--- EOS-OSC.ino
+++ EOS-OSC.ino
@@ -11,29 +11,40 @@ SLIPEncodedUSBSerial SLIPSerial(thisBoardsSerialUSB);
 #include <SLIPEncodedSerial.h>
 SLIPEncodedSerial SLIPSerial(Serial);
 #endif
-#include <string.h>
 
+#include <string.h>
 
 
 #define FORWARD             0
 #define REVERSE             1
 
+// Change these values to switch which direction increase/decrease pan/tilt
 #define PAN_DIR             FORWARD
 #define TILT_DIR            FORWARD
-#define LEVEL_DIR           REVERSE
+#define LEVEL_DIR       REVERSE
 
-#define PAN_SCALE           1
-#define TILT_SCALE          1
-#define LEVEL_SCALE         1
+// Use these values to make the encoder more coarse or fine.
+// This controls the number of wheel "ticks" the device sends to the console
+// for each tick of the encoder. 1 is the default and the most fine setting.
+// Must be an integer.
+#define PAN_SCALE           10
+#define TILT_SCALE          10
+#define LEVEL_SCALE     3
 
-#define VERSION_STRING      "2.0.0.1"
 
-#define PING_AFTER_IDLE_INTERVAL    2500
-#define TIMEOUT_AFTER_IDLE_INTERVAL 5000
-
+#define BOX_NAME_STRING     "box1"
 
 const String HANDSHAKE_QUERY = "ETCOSC?";
 const String HANDSHAKE_REPLY = "OK";
+unsigned long lastMessageRxTime = 0;
+bool timeoutPingSent = false;
+
+// Change these values to alter how long we wait before sending an OSC ping
+// to see if Eos is still there, and then finally how long before we
+// disconnect and show the splash screen
+// Values are in milliseconds
+#define PING_AFTER_IDLE_INTERVAL    2500
+#define TIMEOUT_AFTER_IDLE_INTERVAL 5000
 
 enum WHEEL_TYPE { TILT, PAN, LEVEL};
 enum WHEEL_MODE { COARSE, FINE };
@@ -45,12 +56,13 @@ struct Encoder
   int pinAPrevious;
   int pinBPrevious;
   float pos;
-  bool fine;
   uint8_t direction;
 };
+
 struct Encoder panWheel;
 struct Encoder tiltWheel;
 struct Encoder levelWheel;
+
 enum ConsoleType
 {
   ConsoleNone,
@@ -58,11 +70,7 @@ enum ConsoleType
   ConsoleCobalt,
   ConsoleColorSource
 };
-
 ConsoleType connectedToConsole = ConsoleEos;
-unsigned long lastMessageRxTime = 0;
-bool timeoutPingSent = false;
-
 
 void parseOSCMessage(String& msg)
 {
@@ -73,8 +81,8 @@ void parseOSCMessage(String& msg)
     SLIPSerial.beginPacket();
     SLIPSerial.write((const uint8_t*)HANDSHAKE_REPLY.c_str(), (size_t)HANDSHAKE_REPLY.length());
     SLIPSerial.endPacket();
-
   }
+
 }
 
 
@@ -90,11 +98,7 @@ void initEncoder(struct Encoder* encoder, uint8_t pinA, uint8_t pinB, uint8_t di
 
   encoder->pinAPrevious = digitalRead(pinA);
   encoder->pinBPrevious = digitalRead(pinB);
-
-  encoder-> fine = false;
 }
-
-
 
 int8_t updateEncoder(struct Encoder* encoder)
 {
@@ -114,13 +118,9 @@ int8_t updateEncoder(struct Encoder* encoder)
   }
   encoder->pinAPrevious = pinACurrent;
   encoder->pinBPrevious = pinBCurrent;
-  Serial.begin(9600);
-  Serial.print(encoderMotion);
-  Serial.end();
+
   return encoderMotion;
 }
-
-
 
 void sendOscMessage(const String &address, float value)
 {
@@ -131,95 +131,121 @@ void sendOscMessage(const String &address, float value)
   SLIPSerial.endPacket();
 }
 
-
-
 void sendEosWheelMove(WHEEL_TYPE type, float ticks)
 {
   String wheelMsg("/eos/wheel");
+  if (type == PAN || type == TILT)
+  {
+    /*
+    if (digitalRead(SHIFT_BTN) == LOW)
+      wheelMsg.concat("/fine");
+    else
+      wheelMsg.concat("/coarse");
+      */
+  }
 
 
-  // This is code to enable fine mode when shift (hardlined) is pressed
-  // Disabled until keyboard solution can be created
-  /*
-  if (digitalRead(SHIFT_BTN) == LOW)
-    wheelMsg.concat("/fine");
-  else
-    wheelMsg.concat("/coarse");
-  */
-  
-  // debug 
-  Serial.begin(9600);
   if (type == PAN)
-  {
-    wheelMsg.concat("/fine/pan");
-    Serial.print("pan");
-  }
+    wheelMsg.concat("/coarse/pan");
   else if (type == TILT)
-  {
-    wheelMsg.concat("/fine/tilt");
-    Serial.print("tilt");
-  }
+    wheelMsg.concat("/coarse/tilt");
   else if (type == LEVEL)
-  {
     wheelMsg.concat("/level");
-    Serial.print("level");
-  }
   else
-  {
     // something has gone very wrong
     return;
-  }
-   Serial.end();
+
   sendOscMessage(wheelMsg, ticks);
 }
 
+
+
 void sendWheelMove(WHEEL_TYPE type, float ticks)
 {
-  sendEosWheelMove(type, ticks); 
+  switch (connectedToConsole)
+  {
+    default:
+    case ConsoleEos:
+      sendEosWheelMove(type, ticks);
+  }
 }
 
-void setup() 
-{
-    SLIPSerial.begin(115200);
-    //This is a hack around an Arduino bug. It was taken from the OSC library
-    //examples
-    #ifdef BOARD_HAS_USB_SERIAL
-      while (!SerialUSB)
-    #else 
-      while (!Serial);
-    #endif
+void setup() {
+  SLIPSerial.begin(115200);
+  // This is a hack around an Arduino bug. It was taken from the OSC library
+  //examples
+#ifdef BOARD_HAS_USB_SERIAL
+  while (!SerialUSB);
+#else
+  while (!Serial);
+#endif
 
-     // This is necessary for reconnecting a device because it needs some time
-    // for the serial port to open. The handshake message may have been sent
-    // from the console before #lighthack was ready
+  initEncoder(&panWheel, A0, A1, PAN_DIR);
+  initEncoder(&tiltWheel, A2, A3, TILT_DIR);
+  initEncoder(&levelWheel, A4, A5, LEVEL_DIR);
 
-    SLIPSerial.beginPacket();
-    SLIPSerial.write((const uint8_t*)HANDSHAKE_REPLY.c_str(), (size_t)HANDSHAKE_REPLY.length());
-    SLIPSerial.endPacket();
 
-    initEncoder(&panWheel, A0, A1, PAN_DIR);
-    initEncoder(&tiltWheel, A2, A3, TILT_DIR);
-    initEncoder(&levelWheel, A4, A5, LEVEL_DIR);
 }
 
-void loop() 
-{
+void loop() {
   static String curMsg;
   int size;
-
+  // get the updated state of each encoder
   int32_t panMotion = updateEncoder(&panWheel);
   int32_t tiltMotion = updateEncoder(&tiltWheel);
   int32_t levelMotion = updateEncoder(&levelWheel);
 
+  // Scale the result by a scaling factor
   panMotion *= PAN_SCALE;
   tiltMotion *= TILT_SCALE;
   levelMotion *= LEVEL_SCALE;
 
-  if (panMotion != 0)
-    sendWheelMove(PAN, panMotion);
   if (tiltMotion != 0)
     sendWheelMove(TILT, tiltMotion);
-  if (levelMotion != 0)
-    sendWheelMove(LEVEL, tiltMotion);
 
+  if (panMotion != 0)
+    sendWheelMove(PAN, panMotion);
+
+  if (levelMotion != 0)
+    sendWheelMove(LEVEL, levelMotion);
+
+  size = SLIPSerial.available();
+  if (size > 0)
+  {
+    // Fill the msg with all of the available bytes
+    while (size--)
+      curMsg += (char)(SLIPSerial.read());
+  }
+  if (SLIPSerial.endofPacket())
+  {
+    parseOSCMessage(curMsg);
+    lastMessageRxTime = millis();
+    // We only care about the ping if we haven't heard recently
+    // Clear flag when we get any traffic
+    timeoutPingSent = false;
+    curMsg = String();
+  }
+  if (lastMessageRxTime > 0)
+  {
+    unsigned long diff = millis() - lastMessageRxTime;
+    //We first check if it's been too long and we need to time out
+    if (diff > TIMEOUT_AFTER_IDLE_INTERVAL)
+    {
+      connectedToConsole = ConsoleNone;
+      lastMessageRxTime = 0;
+      timeoutPingSent = false;
+    }
+
+    //It could be the console is sitting idle. Send a ping once to
+    // double check that it's still there, but only once after 2.5s have passed
+    if (!timeoutPingSent && diff > PING_AFTER_IDLE_INTERVAL)
+    {
+      OSCMessage ping("/eos/ping");
+      ping.add(BOX_NAME_STRING "_hello"); // This way we know who is sending the ping
+      SLIPSerial.beginPacket();
+      ping.send(SLIPSerial);
+      SLIPSerial.endPacket();
+      timeoutPingSent = true;
+    }
+  }
 }
